b8d8c91aeb39abfe40b1dc728d13a901873a8122
Bihan Chakraborty <bhchakraborty001@gmail.com>
2023-01-02 22:17:43 +0530

    Time: 0 ms (100%), Space: 59.4 MB (72.51%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int low = 0, high = arr.length - 1;
-        int ans = 0;
+        int low = 0;
+        int high = arr.length - 1;
         while(low < high) {
             int mid = low + (high - low) / 2;
             if(mid == 0 || mid == arr.length - 1) return 0;
@@ -12,6 +12,6 @@ class Solution {
                 high = mid;
             }
         }
-        return 0;
+        return -1;
     }
 }
